6f1cbc952d1dcf6c0a569cb815fce95cb1555842
veera_1505 <veerasaraha@gmail.com>
2021-06-12 19:39:56 +0530

    added welcome page for users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import Login from './component/Login'
 import Signup from './component/Signup'
-import WelcomePgae from './component/WelcomePgae'
+import WelcomePage from './component/WelcomePage'
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Signup} />
           <Route exact path='/login' component={Login} />
-          <Route exact path='/home' component={WelcomePgae} />
+          <Route exact path='/home' component={WelcomePage} />
         </Switch>
       </Router>
     </div>
